cacfb6e2ecfb013c54ae046f6ff7ff0659857b99
Nick Sawe <codekip@outlook.com>
2022-09-24 14:13:04 +0100

    Searching works and input cleared afterwards

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Container } from 'react-bootstrap'
+
 
 const Header = ({ title }) => {
     return (
         <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/">{ title }</Navbar.Brand>
-
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
         </Navbar>
 
     )

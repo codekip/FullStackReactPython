cf56181cd447cd8f235e6c16588ba6667c1713da
Nick Sawe <codekip@outlook.com>
2022-09-24 15:12:18 +0100

    Linter fixed issues in header and search component files

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import { Navbar, Container } from 'react-bootstrap'
-
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 
 const Header = ({ title }) => {
-    return (
-        <Navbar bg="primary" variant="dark">
-            <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-
-    )
-}
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,27 +2,29 @@ import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 export default function Search({ word, setword, handleSubmit }) {
-    return (
-        <Container className="mt-4">
-            <Row className="justify-content-center">
-                <Col xs={12} md={8} lg={8}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Row>
-                            <Col xs={9}>
-                                <Form.Control type="text"
-                                    value={word}
-                                    onChange={(e) => setword(e.target.value)}
-                                    placeholder='Enter search term' />
-                            </Col>
-                            <Col>
-                                <Button variant='primary' type='submit'>
-                                    Submit
-                                </Button>
-                            </Col>
-                        </Form.Row>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={8}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setword(e.target.value)}
+                  placeholder="Enter search term"
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
